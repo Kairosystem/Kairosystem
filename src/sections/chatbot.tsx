0b0b07cbae6cc6987f34e0dd703ff44e1464ed69
Nico-valdes <nicovaldes1104@gmail.com>
2025-04-09 16:34:40 -0300

    robotito diseño

--- src/sections/chatbot.tsx
+++ src/sections/chatbot.tsx
@@ -4,7 +4,7 @@ import { Bot } from "lucide-react"; // Importa el ícono de robot
 
 export default function Chatbot() {
   return (
-    <div className="fixed bottom-5 right-5 w-16 h-16 bg-green-800 rounded-lg flex items-center justify-center shadow-lg cursor-pointer hover:bg-green-600 transition-colors">
+    <div className="fixed bottom-5 right-5 w-16 h-16 bg-[#51E171] rounded-full flex items-center justify-center shadow-lg cursor-pointer hover:bg-[#51E171]/80 transition-colors z-50">
       <Bot className="h-8 w-8 text-white" />
     </div>
   );
