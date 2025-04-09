90e2604976a62c8a75a963ea25a45b2c44fa915c
IvanVijandi <ivijandi75@gmail.com>
2025-04-09 16:23:14 -0300

    Component Bot added

--- src/sections/chatbot.tsx
+++ src/sections/chatbot.tsx
@@ -4,7 +4,7 @@ import { Bot } from "lucide-react"; // Importa el ícono de robot
 
 export default function Chatbot() {
   return (
-    <div className="fixed bottom-5 right-5 w-16 h-16 bg-green-700 rounded-lg flex items-center justify-center shadow-lg cursor-pointer hover:bg-green-800 transition-colors">
+    <div className="fixed bottom-5 right-5 w-16 h-16 bg-green-800 rounded-lg flex items-center justify-center shadow-lg cursor-pointer hover:bg-green-600 transition-colors">
       <Bot className="h-8 w-8 text-white" />
     </div>
   );
