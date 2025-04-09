901995c33b6cbed7550414aaae9ec69be6813fa5
IvanVijandi <ivijandi75@gmail.com>
2025-04-09 16:16:05 -0300

    Bot Component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-
-import Navbar from "@/sections/navbar"
-import ServicesSection from "@/sections/services-section"
-import FeaturesSection from "@/sections/features-section"
-import CTASection from "@/sections/cta-section"
-import Footer from "@/sections/footer"
-import HeroSection from "@/sections/hero-section"
+import Navbar from "@/sections/navbar";
+import ServicesSection from "@/sections/services-section";
+import FeaturesSection from "@/sections/features-section";
+import CTASection from "@/sections/cta-section";
+import Footer from "@/sections/footer";
+import HeroSection from "@/sections/hero-section";
+import Chatbot from "@/sections/chatbot";
 export default function Home() {
   return (
     <div className="min-h-screen bg-black text-white">
@@ -13,7 +13,8 @@ export default function Home() {
       <ServicesSection />
       <FeaturesSection />
       <CTASection />
+      <Chatbot />
       <Footer />
     </div>
-  )
+  );
 }
---
+++ src/sections/chatbot.tsx
@@ -0,0 +1,11 @@
+"use-client";
+import React from "react";
+import { Bot } from "lucide-react"; // Importa el ícono de robot
+
+export default function Chatbot() {
+  return (
+    <div className="fixed bottom-5 right-5 w-16 h-16 bg-green-700 rounded-lg flex items-center justify-center shadow-lg cursor-pointer hover:bg-green-800 transition-colors">
+      <Bot className="h-8 w-8 text-white" />
+    </div>
+  );
+}
